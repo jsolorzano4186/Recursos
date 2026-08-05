da0e6c80e0a01fc899901c22d925175c443a1e80
jsolorzano4186 <jsolorzano4186@utm.edu.ec>
2026-08-05 18:33:40 -0500

    fix: conexion a mongodb atlas configurada

--- config/db.js
+++ config/db.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const dns = require('dns');
+
+// Forzar la resolución DNS para evitar el bloqueo ECONNREFUSED
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const connectDB = async () => {
     try {
